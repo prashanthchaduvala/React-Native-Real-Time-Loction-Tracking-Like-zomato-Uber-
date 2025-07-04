90aff3a2d4e76d736e927dd1344cd6ec5e95b918
Prashanth Chaduvala <prashanth.chaduvala@cdatechindia.com>
2025-07-04 17:03:36 +0530

    added map

--- utils/api.js
+++ utils/api.js
@@ -22,9 +22,9 @@ import axios from 'axios';
 // });
 
 const API = axios.create({
-    baseURL: 'https://api-test3.cdaitsolutions.com',
+    // baseURL: 'https://api-test3.cdaitsolutions.com',
     // baseURL:'http://192.168.231.85:6001/',
-    // baseURL: 'http://127.0.0.1:6001/',
+    baseURL: 'http://127.0.0.1:6001/',
 });
 
 export let authToken = null;
