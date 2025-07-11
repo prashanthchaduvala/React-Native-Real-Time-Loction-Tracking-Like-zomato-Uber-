e6609a8ec085c4ced764057dcdefc16853a99695
prashanthch <54631863+prashanthchaduvala@users.noreply.github.com>
2025-07-11 16:11:24 +0530

    Update api.js
    removed backend link

--- utils/api.js
+++ utils/api.js
@@ -1,45 +1,3 @@
-// utils/api.js
-// import axios from 'axios';
-
-// const API = axios.create({
-//   baseURL: 'http://127.0.0.1:6001/',
-// });
-
-// export let authToken = null;
-
-// export const setToken = (token) => {
-//   authToken = token;
-//   API.defaults.headers.common['Authorization'] = `Token ${token}`;
-// };
-
-// export default API;
-
-// import axios from 'axios';
-
-// // const API = axios.create({
-// //   baseURL: 'http://127.0.0.1:6001/',
-// //   // baseURL:'http://192.168.231.85:6001/', 
-// // });
-
-// const API = axios.create({
-//     baseURL: 'https://api-test3.cdaitsolutions.com',
-//     // baseURL:'http://192.168.231.85:6001/',
-//     // baseURL: 'http://127.0.0.1:6001/',
-// });
-
-// export let authToken = null;
-
-// export const setToken = (token) => {
-//   authToken = token;
-//   if (token) {
-//     API.defaults.headers.common['Authorization'] = `Token ${token}`;
-//   } else {
-//     delete API.defaults.headers.common['Authorization'];
-//   }
-// };
-
-// export default API;
-
 
 
 // utils/api.js
@@ -47,7 +5,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const API = axios.create({
-  baseURL: 'https://api-test3.cdaitsolutions.com',
+  baseURL: '',
 });
 
 export const loadToken = async () => {
